50dc6b522e78d040511a5b98d5b0d569cf1c770a
sidharthbhasin <sidharth.bhasin@sjsu.edu>
2017-03-25 23:00:29 -0700

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -5,7 +5,25 @@ public class Application{
   public static void main(String args[]){
     
     Parser parser = new Parser();
+    // we have to enter a code input here.
+    // Let that input be from a '.java' file.
+    //Then we first have to read the content of that file and then have put that in a variable.
+    StringBuilder sb = new StringBuilder();
+
+    BufferedReader br = new BufferedReader(new FileReader("file.txt"));
+    try {
+        String line = br.readLine();
+
+        while (line != null) {
+            sb.append(line);
+            sb.append(System.lineSeparator());
+            line = br.readLine();
+        }
+        String everything = sb.toString();
+    } finally {
+        br.close();
+    }
+    // sb has the file code now.
     
-  
   }
 }
