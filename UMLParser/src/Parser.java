41c0fd8cc69023602b7b729203fe9103b2ee6168
sidharthbhasin <sidharth.bhasin@sjsu.edu>
2017-04-08 12:41:58 -0700

    Update Parser.java

--- UMLParser/src/Parser.java
+++ UMLParser/src/Parser.java
@@ -12,52 +12,45 @@ import japa.parser.ast.CompilationUnit;
 
 public class Parser implements Methods{
 
-  public void parseCode(String code){
-    
-  }
+	  public void parseCode(String code){}
+	  public boolean validateCode(String code){}
+	  public void divideCode(String code){}
   
-  public boolean validateCode(String code){
-  
-  }
-  
-  public void divideCode(String code){
-  
-  }
-  
-  ArrayList<CompilationUnit> currentArray;
-	String inputPath;
-	
-	String outputPath;
-	ParserEngine(String inputPath, String outputPath){
+	  ArrayList<CompilationUnit> currentArray;
+	  String inputPath;	
+	  String outputPath;
+
+	  ParserEngine(String inputPath, String outputPath){
 		this.inputPath=inputPath;
 		this.outputPath=inputPath + "\\" + outputPath + ".png";
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	  }
 
-	}
+	  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	  }
+
 	public void start() throws Exception {
 		currentArray = readFileFromFolder(inputPath);
-    System.out.println("Code Started");
-    }
+		System.out.println("Code Started");
+	}
   
-    private String parser() {
-	String result = "";
-	String className = "";
-	        
-	return " ";
-    }
+        private String parser() {
+	    String result = "";
+	    String className = "";
+
+            return " ";
+        }
 	  
-    private String aToSymScope(String stringScope) {
-	switch (stringScope) {
-	    case "private":
-	        return "-";
-	    case "public":
-	            return "+";
-	        default:
-	            return "";
-	        }
-	    }
+	private String aToSymScope(String stringScope) {
+		switch (stringScope) {
+		    case "private":
+			return "-";
+		    case "public":
+			    return "+";
+			default:
+			    return "";
+			}
+		    }
 	
 	public ArrayList<CompilationUnit> readFileFromFolder(String location){
 	  ArrayList<CompilationUnit> allUnits= new ArrayList<CompilationUnit>();
